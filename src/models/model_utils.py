fce939eea73f7aa754269995c7ad82251e7ade8e
Kirill Rodriguez <theoden8@tutamail.com>
2021-04-13 20:56:10 +0100

    model loading code shared between two model types

--- src/models/model_utils.py
+++ src/models/model_utils.py
@@ -5,8 +5,14 @@ import os
 import sys
 
 import requests
+import numpy as np
 import pandas as pd
 import typing
+from overrides import overrides
+from abc import abstractmethod
+
+import spacy
+import allennlp
 
 import torch
 import torch.utils
@@ -16,11 +22,12 @@ import transformers
 # add project root directory to the search path
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..')))
 from src.data.dataload import *
+import notebooks.AllenNLP.BCN_model as BCN_model
 
 
 class Model:
     def __init__(self):
-        pass
+        self.model = None
 
     def _download_if_not_exists(self, url: str, filepath: str, override=False) -> str:
         if os.path.isfile(filepath) and not override:
@@ -31,16 +38,92 @@ class Model:
             print('download finished')
         return filepath
 
+    def _get_model_url_for_dataset(self, dataset) -> typing.Optional[str]:
+        return None
+
+    @abstractmethod
+    def _get_model_filepath_for_dataset(self, dataset) -> str:
+        pass
+
+    @abstractmethod
+    def _load_pretrained_model(self, filepath: str) -> None:
+        pass
+
+    def _load_model_from_url(self, url: str, filepath: str, override=False) -> None:
+        self._download_if_not_exists(url=url, filepath=filepath, override=override)
+        self._load_pretrained_model(filepath)
+
+    def _load_model_from_dataset(self, dataset, override=False) -> None:
+        filepath = self._get_model_filepath_for_dataset(dataset)
+        url = self._get_model_url_for_dataset(dataset)
+        if url is None:
+            self._load_pretrained_model(filepath=filepath)
+        else:
+            self._load_model_from_url(url=url, filepath=filepath, override=override)
+
+    def load_model(self, dataset) -> None:
+        self._load_model_from_dataset(dataset)
+
+
+@allennlp.predictors.predict.Predictor.register('ag_text_classifier')
+class AGNewsClassifier(allennlp.predictors.predict.Predictor):
+    """
+    Predictor for any model that takes in a sentence and returns
+    a single class for it.  In particular, it can be used with
+    the [`BasicClassifier`](../models/basic_classifier.md) model.
+
+    """
+
+    def predict(self, sentence: str) -> allennlp.common.util.JsonDict:
+        return self.predict_json({"Description": sentence})
+
+    @overrides
+    def _json_to_instance(self, json_dict: allennlp.common.util.JsonDict) -> allennlp.data.Instance:
+        """
+        Expects JSON that looks like `{"sentence": "..."}`.
+        Runs the underlying model, and adds the `"label"` to the output.
+        """
+        sentence = json_dict["Description"]
+        reader_has_tokenizer = (
+            getattr(self._dataset_reader, "tokenizer", None) is not None
+            or getattr(self._dataset_reader, "_tokenizer", None) is not None
+        )
+        if not reader_has_tokenizer:
+            tokenizer = allennlp.data.tokenizers.spacy_tokenizer.SpacyTokenizer()
+            sentence = tokenizer.tokenize(sentence)
+        return self._dataset_reader.text_to_instance(sentence)
+
+    @overrides
+    def predictions_to_labeled_instances(self,
+            instance: allennlp.data.Instance,
+            outputs: typing.Dict[str, np.ndarray]) \
+                -> typing.List[allennlp.data.Instance]:
+        new_instance = instance.duplicate()
+        label = np.argmax(outputs["class_probabilities"])
+        new_instance.add_field("label", allennlp.data.fields.LabelField(int(label), skip_indexing=True))
+        return [new_instance]
+
+
 
 def BCNModel(Model):
-    def __init__(self):
-        pass
+    def __init__(self, cache_dir='.'):
+        self.nlp = spacy.load('en_core_web_sm')
+        self.model = None
+        self.vocab = None
+        self.cache_dir = cache_dir
+
+    def _get_model_filepath_for_dataset(self, dataset) -> str:
+        return os.path.join(self.cache_dir, f'{dataset.NAME}-bcn.tar.gz')
+
+    def _load_pretrained_model(self, filepath: str) -> None:
+        archive = allennlp.model.archival.load_archive(filepath)
+        self.model = archive.model
+        self.vocab = self.model.vocab
 
 
 BERT_BASE, BERT_LARGE = 'base', 'large'
 TOKENIZER_UNCASED = 'uncased'
 class BERTModel(Model):
-
     def __init__(self, cache_dir='.', bert_type=BERT_BASE, tokenizer_type=TOKENIZER_UNCASED) -> None:
         assert bert_type in [BERT_BASE, BERT_LARGE]
         self.bert_type = bert_type
@@ -65,26 +148,11 @@ class BERTModel(Model):
         filename = f'fine-tuned-bert-{self.bert_type}-{dataset.NAME.lower()}'
         return os.path.join(self.cache_dir, filename)
 
-    def _load_pretrained_bert(self, filepath: str) -> None:
+    def _load_pretrained_model(self, filepath: str) -> None:
         self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
         self.model = transformers.BertForSequenceClassification \
                                  .from_pretrained(filepath)
         self.model.to(self.device)
-
-    def _load_model_from_url(self, url: str, filepath: str) -> None:
-        self._download_if_not_exists(url=url, filepath=filepath)
-        self._load_pretrained_bert(filepath)
-
-    def _load_model_from_dataset(self, dataset, override=False) -> None:
-        filepath = self._get_model_filepath_for_dataset(dataset)
-        url = self._get_model_url_for_dataset(dataset)
-        if url is None:
-            self._load_pretrained_bert(filepath=filepath)
-        else:
-            self._load_from_url(url=url, filepath=filepath)
-
-    def load_model(self, dataset) -> None:
-        self._load_model_from_dataset(dataset)
 
     def _load_tokenizer(self, do_lower_case=True, **kwargs) -> None:
         self.tokenizer = transformers.BertTokenizer \
